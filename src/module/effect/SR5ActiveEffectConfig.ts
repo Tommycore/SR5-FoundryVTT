c67cdf26169a20bcd68bb444c94930f7c9609846
Jan Schoska <jan.schoska@gmail.com>
2022-09-04 13:06:59 +0200

    Removed dev code

--- src/module/effect/SR5ActiveEffectConfig.ts
+++ src/module/effect/SR5ActiveEffectConfig.ts
@@ -6,7 +6,6 @@ export class SR5ActiveEffectConfig extends ActiveEffectConfig {
     getData(options?: Application.RenderOptions): Promise<ActiveEffectConfig.Data> | ActiveEffectConfig.Data {
         const data = super.getData(options) as any;
         data.modes = {...data.modes, 0: game.i18n.localize('SR5.ActiveEffect.Modes.Modify')};
-        console.error('AESheets', data);
         return data;
     }
 }
